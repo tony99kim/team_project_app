b6ee5a6c7eaddd1bf71314ee3f95106c2a95e915
tony99kim <108780666+tony99kim@users.noreply.github.com>
2024-04-03 23:33:38 +0900

    앱 기본 화면 /태엽 오류잡는중 1

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
@@ -48,4 +49,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 }
+
 
