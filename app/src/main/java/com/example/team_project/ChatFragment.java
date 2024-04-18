060db555e8ecc51fafe40a4f721989e8c45e4726
jaehyeong13 <165352835+jaehyeong13@users.noreply.github.com>
2024-04-19 01:24:51 +0900

    재형 test 5

--- app/src/main/java/com/example/team_project/ChatFragment.java
+++ app/src/main/java/com/example/team_project/ChatFragment.java
@@ -4,18 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ChatFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.chat_room, container, false);
 
+        Button enterButton = view.findViewById(R.id.button_enter);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // FragmentChat으로 이동
+                FragmentChat fragmentChat = new FragmentChat();
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.fragment_container, fragmentChat);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
-
-
