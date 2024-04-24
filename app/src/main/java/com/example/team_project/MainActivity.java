a5c3f6e0eb9c5a2b716344a89f15d789cd85e8ef
ghzzang <nettrol13@naver.com>
2024-04-25 01:10:31 +0900

    마이페이지 프로필 보기 클릭시 사용자 이름 변경 기능 추가 + 구분선 추가 + 최근 방문 기능 추가 + 스크롤바 기능 추가

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -2,21 +2,50 @@ package com.example.team_project;
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.example.team_project.Board.BoardFragment;
+import com.example.team_project.Chat.FragmentChat;
+import com.example.team_project.Home.HomeFragment;
+import com.example.team_project.Home.HomeSettingsFragment;
+import com.example.team_project.Home.NotificationsFragment;
+import com.example.team_project.Home.SearchFragment;
+import com.example.team_project.Profile.ProfileFragment;
+import com.example.team_project.Store.StoreFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.view.MenuItem;
 
-
-//민지 test1
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        // 액션바 설정
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 활성화
+        }
+
+        bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // 프래그먼트 매니저에 리스너 추가
+        getSupportFragmentManager().addOnBackStackChangedListener(() -> {
+            Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            if (currentFragment instanceof HomeSettingsFragment || currentFragment instanceof SearchFragment || currentFragment instanceof NotificationsFragment) {
+                // 특정 프래그먼트에서 하단 메뉴바 숨기기
+                hideBottomNav();
+            } else {
+                // 그 외의 경우 하단 메뉴바 보이기
+                showBottomNav();
+            }
+        });
 
         // 앱이 시작될 때 기본으로 보여줄 프래그먼트를 설정합니다.
         if (savedInstanceState == null) {
@@ -30,14 +59,15 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
 
-
                     int itemId = item.getItemId();
                     if (itemId == R.id.navigation_home) {
                         selectedFragment = new HomeFragment();
+                    } else if (itemId == R.id.navigation_store) {
+                        selectedFragment = new StoreFragment();
                     } else if (itemId == R.id.navigation_board) {
                         selectedFragment = new BoardFragment();
                     } else if (itemId == R.id.navigation_chat) {
-                        selectedFragment = new ChatFragment();
+                        selectedFragment = new FragmentChat();
                     } else if (itemId == R.id.navigation_profile) {
                         selectedFragment = new ProfileFragment();
                     }
@@ -47,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getSupportFragmentManager().popBackStack(); // 뒤로가기 스택에서 이전 프래그먼트로 돌아가기
+        return true;
+    }
+
+    private void hideBottomNav() {
+        bottomNav.setVisibility(BottomNavigationView.GONE); // 하단 메뉴바 숨기기
+    }
+
+    private void showBottomNav() {
+        bottomNav.setVisibility(BottomNavigationView.VISIBLE); // 하단 메뉴바 보이기
+    }
 }
