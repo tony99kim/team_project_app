91671f1e854ae72d8fb9a27721d8882f13e287fa
jaehyeong13 <165352835+jaehyeong13@users.noreply.github.com>
2024-05-18 05:03:10 +0900

    검색기능 박재형

--- app/src/main/java/com/example/team_project/Board/BoardFragmentPagerAdapter.java
+++ app/src/main/java/com/example/team_project/Board/BoardFragmentPagerAdapter.java
@@ -5,11 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.example.team_project.Board.BoardKategorie.BoardEventFragment;
-import com.example.team_project.Board.BoardKategorie.BoardFreeFragment;
-import com.example.team_project.Board.BoardKategorie.BoardNewsFragment;
-import com.example.team_project.Board.BoardKategorie.BoardVolunteerFragment;
-
 public class BoardFragmentPagerAdapter extends FragmentStateAdapter {
     private static final int NUM_PAGES = 4; // 페이지 수
 
