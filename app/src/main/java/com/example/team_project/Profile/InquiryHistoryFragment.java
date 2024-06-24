95b9b4785f5e1416bc62707c3ca8a37914170929
ghzzang <nettrol13@naver.com>
2024-06-25 03:13:43 +0900

    문의 전환 기능 구현 / 가현

--- app/src/main/java/com/example/team_project/Profile/InquiryHistoryFragment.java
+++ app/src/main/java/com/example/team_project/Profile/InquiryHistoryFragment.java
@@ -12,10 +12,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.team_project.R;
+import com.google.android.material.button.MaterialButton;
 
 public class InquiryHistoryFragment extends Fragment {
 
     private View rootView;
+    private MaterialButton btnBackToInquiry;
 
     @Nullable
     @Override
@@ -29,6 +31,16 @@ public class InquiryHistoryFragment extends Fragment {
             activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             activity.getSupportActionBar().setTitle("문의 내역 확인");
         }
+
+        // 뒤로 가기 버튼 클릭 리스너
+        btnBackToInquiry = rootView.findViewById(R.id.btn_back_to_inquiry);
+        btnBackToInquiry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 문의하기 화면으로 돌아가기
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
 
         return rootView;
     }
